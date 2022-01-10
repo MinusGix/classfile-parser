aed4640dece3e1e063eb7565843fe786f78a1171
MinusGix <MinusGix@gmail.com>
2022-01-09 18:02:02 -0600

    Make DescriptorType::parse operate on bytes rather than utf8

--- src/descriptor/types.rs
+++ src/descriptor/types.rs
@@ -68,28 +68,33 @@ impl<'a> DescriptorType<'a> {
     }
 
     pub fn parse(mut text: &'a str) -> Result<(DescriptorType<'a>, &'a str), DescriptorTypeError> {
-        let mut iter = text.char_indices().peekable();
-        let (latest_index, first) = iter.next().ok_or(DescriptorTypeError::NoInput)?;
-        let mut latest_index = latest_index + first.len_utf8();
+        // We use bytes here because this lets us avoid the slightly expensive utf8 iteration
+        // Which is thankfully correct since a utf8 character won't have parts that look like ASCII
+        // which we use to detect the start and end of parts
+        // Though, if this panics, then that means we did it incorrectly.
+
+        let mut iter = text.as_bytes().iter();
+        let first = iter.next().ok_or(DescriptorTypeError::NoInput)?;
+        let mut latest_index = 1;
 
         let value: DescriptorType = match first {
-            'B' => DescriptorTypeBasic::Byte.into(),
-            'C' => DescriptorTypeBasic::Char.into(),
-            'D' => DescriptorTypeBasic::Double.into(),
-            'F' => DescriptorTypeBasic::Float.into(),
-            'I' => DescriptorTypeBasic::Int.into(),
-            'J' => DescriptorTypeBasic::Long.into(),
-            'L' => {
+            b'B' => DescriptorTypeBasic::Byte.into(),
+            b'C' => DescriptorTypeBasic::Char.into(),
+            b'D' => DescriptorTypeBasic::Double.into(),
+            b'F' => DescriptorTypeBasic::Float.into(),
+            b'I' => DescriptorTypeBasic::Int.into(),
+            b'J' => DescriptorTypeBasic::Long.into(),
+            b'L' => {
                 let start_index = latest_index;
                 let mut prev_index = start_index;
                 let mut found_semicolon = false;
-                for (i, c) in iter {
-                    latest_index = i + c.len_utf8();
-                    if c == ';' {
+                for c in iter {
+                    latest_index += 1;
+                    if *c == b';' {
                         found_semicolon = true;
                         break;
                     }
-                    prev_index = i + c.len_utf8();
+                    prev_index += 1;
                 }
 
                 if !found_semicolon {
@@ -104,14 +109,14 @@ impl<'a> DescriptorType<'a> {
 
                 DescriptorTypeBasic::ClassName(Cow::Borrowed(class_name)).into()
             }
-            'S' => DescriptorTypeBasic::Short.into(),
-            'Z' => DescriptorTypeBasic::Boolean.into(),
-            '[' => {
+            b'S' => DescriptorTypeBasic::Short.into(),
+            b'Z' => DescriptorTypeBasic::Boolean.into(),
+            b'[' => {
                 // TODO: NonZeroUsize ops would make this a bit nicer
                 let mut level = 1usize;
-                for (i, c) in iter {
-                    if c == '[' {
-                        latest_index = i + c.len_utf8();
+                for c in iter {
+                    if *c == b'[' {
+                        latest_index += 1;
                         level = level
                             .checked_add(1)
                             .ok_or(DescriptorTypeError::TooManyNestedArrays)?;
@@ -134,6 +139,7 @@ impl<'a> DescriptorType<'a> {
             _ => return Err(DescriptorTypeError::InvalidTypeOpener),
         };
 
+        // If we got here with a latest index of 1 then it has to be a singular ascii character
         text = &text[latest_index..];
         Ok((value, text))
     }
